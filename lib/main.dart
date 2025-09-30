e2187a5b71b1a8fe271482fc63250d211a01bfe0
Samsomn <soloindiedevs@gmail.com>
2025-09-30 10:29:51 +0800

    feat: Add Credit card payment implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'services/checkout_service.dart';
+import 'services/credit_card_payment.dart';
 
 void main() {
   print('Demonstrating Dependency Injection with Gateways');
+
+  print('   Customer is paying with Credit Card    ');
+  var creditCardGateway = CreditCardPayment('4111-1111-1111-1111');
+  var checkoutWithCard = CheckoutService(creditCardGateway);
+  checkoutWithCard.completeCheckout(150.75);
+  checkoutWithCard.issueRefund(25.00);
 }
